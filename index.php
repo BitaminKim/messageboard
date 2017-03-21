a6be0059bdbd1d0ce17aa8024ef3cc45339ec31c
BitaminKim <BitaminKim@users.noreply.github.com>
2017-03-21 11:24:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         #content {width:45%;float: left;}
 	</style>
 </head>
-<body background="img/back-ground.jpg">
+<body>
   <div id="Layer1" style="position:absolute; width:100%; height:100%; z-index:-1">    
     <img src="img/back-ground.jpg" height="100%" width="100%"/>    
   </div>
